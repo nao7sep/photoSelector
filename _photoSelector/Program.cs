9183481856d9086e063da5e40bd4fe1861ca9ea7
Yoshinao Inoguchi <yoshinao@inoguchi.com>
2024-11-22 22:41:45 +0900

    Initially

---
+++ _photoSelector/Program.cs
@@ -0,0 +1,27 @@
+﻿using yyLib;
+
+namespace _photoSelector
+{
+    class Program
+    {
+        static void Main (string [] args)
+        {
+            try
+            {
+            }
+
+            catch (Exception xException)
+            {
+                yyLogger.Default.TryWriteException (xException);
+                Console.WriteLine (xException.ToString ());
+            }
+
+            finally
+            {
+                Console.Write ("Press any key to exit: ");
+                Console.ReadKey (true);
+                Console.WriteLine ();
+            }
+        }
+    }
+}
